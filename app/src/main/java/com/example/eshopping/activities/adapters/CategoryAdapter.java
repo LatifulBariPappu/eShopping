82b8d02d894ebcc2c739279f2b3b9a50cc81798b
Latiful_Bari_Pappu <latifulbari2001@gmail.com>
2024-01-31 00:58:39 +0600

    viewHolder

--- app/src/main/java/com/example/eshopping/activities/adapters/CategoryAdapter.java
+++ app/src/main/java/com/example/eshopping/activities/adapters/CategoryAdapter.java
@@ -1,28 +1,43 @@
 package com.example.eshopping.activities.adapters;
 
+import android.content.Context;
+import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
 import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
 
+import com.example.eshopping.R;
+import com.example.eshopping.activities.model.Category;
 import com.example.eshopping.databinding.ItemCategoriesBinding;
 
+import java.util.ArrayList;
+
 public class CategoryAdapter extends RecyclerView.Adapter<CategoryAdapter.CategoryViewHolder>{
+    Context context;
+    ArrayList<Category> categories;
+
+    public CategoryAdapter(Context context, ArrayList<Category> categories) {
+        this.context = context;
+        this.categories = categories;
+    }
+
     @NonNull
     @Override
     public CategoryViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
-        return null;
+        return new CategoryViewHolder(LayoutInflater.from(context).inflate(R.layout.item_categories,parent,false));
     }
 
     @Override
     public void onBindViewHolder(@NonNull CategoryViewHolder holder, int position) {
-
+        Category category=categories.get(position);
+        holder.binding.lebel.setText(category.getName());
     }
 
     @Override
     public int getItemCount() {
-        return 0;
+        return categories.size();
     }
 
     public class CategoryViewHolder extends RecyclerView.ViewHolder{
