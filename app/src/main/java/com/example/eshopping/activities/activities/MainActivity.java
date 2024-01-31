4da412f7b25507cee3e267b0f49e1dce28d7200f
Latiful_Bari_Pappu <latifulbari2001@gmail.com>
2024-01-31 12:16:39 +0600

    recyclerview

--- app/src/main/java/com/example/eshopping/activities/activities/MainActivity.java
+++ app/src/main/java/com/example/eshopping/activities/activities/MainActivity.java
@@ -1,16 +1,32 @@
 package com.example.eshopping.activities.activities;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.GridLayoutManager;
 
 import android.os.Bundle;
 
 import com.example.eshopping.R;
+import com.example.eshopping.activities.adapters.CategoryAdapter;
+import com.example.eshopping.activities.model.Category;
+import com.example.eshopping.databinding.ActivityMainBinding;
+
+import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
 
+    ActivityMainBinding binding;
+    CategoryAdapter categoryAdapter;
+    ArrayList<Category> categories;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        categories=new ArrayList<>();
+        categoryAdapter=new CategoryAdapter(this,categories);
+
+        GridLayoutManager layoutManager=new GridLayoutManager(this,4);
+        binding.categoriesList.setLayoutManager(layoutManager);
+        binding.categoriesList.setAdapter(categoryAdapter);
     }
 }
--- app/src/main/java/com/example/eshopping/activities/adapters/CategoryAdapter.java
+++ app/src/main/java/com/example/eshopping/activities/adapters/CategoryAdapter.java
@@ -1,6 +1,7 @@
 package com.example.eshopping.activities.adapters;
 
 import android.content.Context;
+import android.graphics.Color;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -8,6 +9,7 @@ import android.view.ViewGroup;
 import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
 
+import com.bumptech.glide.Glide;
 import com.example.eshopping.R;
 import com.example.eshopping.activities.model.Category;
 import com.example.eshopping.databinding.ItemCategoriesBinding;
@@ -33,6 +35,11 @@ public class CategoryAdapter extends RecyclerView.Adapter<CategoryAdapter.Catego
     public void onBindViewHolder(@NonNull CategoryViewHolder holder, int position) {
         Category category=categories.get(position);
         holder.binding.lebel.setText(category.getName());
+        Glide.with(context)
+                .load(category.getIcon())
+                .into(holder.binding.image);
+        holder.binding.image.setBackgroundColor(Color.parseColor(category.getColor()));
+
     }
 
     @Override
